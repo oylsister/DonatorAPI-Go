3dc3fcf5cb826deb68354a0bf397fa85640aad9a
Oylsister <57207701+oylsister@users.noreply.github.com>
2025-01-05 22:24:07 +0700

    Added Create User Info

--- main.go
+++ main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    db, err := connectToDB()
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	db, err := connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    router := gin.Default()
+	router := gin.Default()
 
-    router.GET("/userinfo", func(c *gin.Context) {
-        getUserInfo(c, db)
-    })
+	router.GET("/userinfo", func(c *gin.Context) {
+		getUserInfo(c, db)
+	})
 
 	router.GET("/userinfo/:steamAuth", func(c *gin.Context) {
-        getUserInfo(c, db)
-    })
+		getUserInfo(c, db)
+	})
 
-    router.Run(":8080") // Run on port 8080
-    fmt.Println("Server is running on port 8080...")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	router.POST("/userinfo", func(c *gin.Context) {
+		createUserInfo(c, db)
+	})
+
+	router.Run(":8080") // Run on port 8080
+	fmt.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- userinfo.go
+++ userinfo.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-    "database/sql"
-    "net/http"
+	"database/sql"
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 // Json can be rename to something else
 type UserInfo struct {
-    UserID     		int       `json:"id"`
-    UserAuth   		string    `json:"auth"`
-    DonateTier   	string    `json:"donateTier"`
-    ExpireTime 		sql.NullString    `json:"expireTime"`
+	UserID     int            `json:"id"`
+	UserAuth   string         `json:"auth"`
+	DonateTier string         `json:"donateTier"`
+	ExpireTime sql.NullString `json:"expireTime"`
 }
 
-func getUserInfo(c * gin.Context, db *sql.DB) {
+func getUserInfo(c *gin.Context, db *sql.DB) {
 	steamAuth := c.Param("steamAuth")
 
 	// if steamAuth is not empty or null
-	if(steamAuth != "") {
+	if steamAuth != "" {
 		var user UserInfo
 		err := db.QueryRow("SELECT user_id, user_auth, donate_tier, expire_time FROM userinfo WHERE user_auth = ?", steamAuth).Scan(&user.UserID, &user.UserAuth, &user.DonateTier, &user.ExpireTime)
-		
-		if(err != nil) {
+
+		if err != nil {
 			if err == sql.ErrNoRows {
 				c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 			} else {
@@ -35,22 +37,41 @@ func getUserInfo(c * gin.Context, db *sql.DB) {
 		rows, err := db.Query("SELECT user_id, user_auth, donate_tier, expire_time FROM userinfo")
 
 		// nil mean success I guess
-		if (err != nil) {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()}) 
-				return 
-		} 
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 		defer rows.Close()
 
 		var users []UserInfo
 		for rows.Next() {
 			var user UserInfo
-			if err := rows.Scan(&user.UserID, &user.UserAuth, &user.DonateTier, &user.ExpireTime); err != nil { 
-				c.JSON(http.StatusInternalServerError, gin.H {"error": err.Error()}) 
-				return 
+			if err := rows.Scan(&user.UserID, &user.UserAuth, &user.DonateTier, &user.ExpireTime); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
 			}
 
 			users = append(users, user)
 		}
 		c.JSON(http.StatusOK, users)
 	}
+}
+
+func createUserInfo(c *gin.Context, db *sql.DB) {
+	var user UserInfo
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	query := "INSERT INTO userinfo (user_auth, donate_tier, expire_time) VALUES (?, ?, ?)"
+	_, err := db.Exec(query, user.UserAuth, user.DonateTier, user.ExpireTime)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": fmt.Sprintf("User %s has been created", user.UserAuth)})
 }
